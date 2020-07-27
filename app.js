07381a67eadc1104968c49bac72d424550d24e7d
sabbirosa <sabbir0727@gmail.com>
2020-07-27 16:11:38 +0600

    Project completed!

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
-const generatePIN = document.getElementById("generateButton");
-generatePIN.addEventListener("click", function() {
-    var pinNumber = Math.floor(1000 + Math.random() * 9000);
+// Generating PIN and Showing in Display
 
-    showPIN.value = pinNumber;
-})
+const generatePIN = document.getElementById("generateButton");
+
+    generatePIN.addEventListener("click", function() {
+        var pinNumber = Math.floor(1000 + Math.random() * 9000);
+        showPIN.value = pinNumber;
+    })
+
+
+// Getting Number and Showing in Display
+
+const displayNumber = document.getElementById('display').value;
+
+    // Inserting Number
+    function insert(number) {
+        document.getElementById('display').value = document.getElementById('display').value + number; 
+    }
+
+    // Clean Display
+    function clean() {
+        const clean = "";
+        document.getElementById('display').value = clean;  
+    }
+    
+    // Deleting Last Number
+    function back() {
+        const currentValue = document.getElementById("display").value;
+        document.getElementById("display").value = currentValue.substr(0, currentValue.length - 1);
+    }
+
+
+// Matching PIN and Showing Message
+
+    const pinSubmitBtn = document.getElementById("submitBtn");
+    pinSubmitBtn.addEventListener("click", function() {
+        pinTyped = parseInt(display.value);
+
+        let generatedPin =  document.getElementById("showPIN").value;
+        generatedPinNumber = parseInt(generatedPin)
+
+        
+        
+        if (pinTyped === generatedPinNumber) {
+            document.getElementById("notifyCorrect").style.display = "block";
+            document.getElementById("notifyError").style.display = "none";
+        }
+
+        else {
+            document.getElementById("notifyCorrect").style.display = "none";
+            document.getElementById("notifyError").style.display = "block";
+
+        }
+
+    })
